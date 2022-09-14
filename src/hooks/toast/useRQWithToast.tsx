4ca24f21b22642b721995258a68b7e71638d40f2
rizqitsani <rizqitsani@gmail.com>
2022-09-14 16:58:11 +0700

    chore: upgrade react-query to v4

--- src/hooks/toast/useRQWithToast.tsx
+++ src/hooks/toast/useRQWithToast.tsx
@@ -1,6 +1,6 @@
+import { UseQueryResult } from '@tanstack/react-query';
 import * as React from 'react';
 import toast from 'react-hot-toast';
-import { UseQueryResult } from 'react-query';
 
 import { DEFAULT_TOAST_MESSAGE } from '@/constant/toast';
 
